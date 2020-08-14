e8d7c383302ca9bf244b507259c0726952882d42
sahilasopa <sahilasopa12@gmail.com>
2020-08-14 15:43:29 +0530

    commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -10,8 +10,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'whfd51$t(bluz(f#akirc&ugf6v^td62+%%rm+_@!k0uqj0fx('
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
