8b0eff2d88b71d6ce62d30beae080a71465e9388
sahilasopa <sahilasopa12@gmail.com>
2020-08-17 21:45:26 +0530

    Commit

--- static/ckeditor/ckeditor/config.js
+++ static/ckeditor/ckeditor/config.js
@@ -1,10 +1,8 @@
-/**
- * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
- */
-
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-};
+CKEDITOR.config.toolbar = [
+   ['Styles','Format','Font','FontSize'],
+   '/',
+   ['Bold','Italic','Underline','StrikeThrough','-','Undo','Redo','-','Cut','Copy','Paste','Find','Replace','-','Outdent','Indent','-','Print'],
+   '/',
+   ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+   ['Image','Table','-','Link','Flash','Smiley','TextColor','BGColor','Source']
+] ;
